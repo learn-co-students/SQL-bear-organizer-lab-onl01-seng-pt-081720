5c8cdffa6971d01094f08e09871372a4637acd2b
Cloudymolecule <baires82@gmail.com>
2020-10-12 20:23:20 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, "male", "blue", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 10, "male", "red", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 12, "female", "pink", "relaxed", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, "male", "black", "stressed", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, "female", "yellow", "humorous", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 8, "male", "green", "mischivous", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 25, "female", "maroon", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 13, "male", "white", "boring", 0);
