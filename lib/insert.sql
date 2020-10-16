3cd24cc357119afc890a5707cea52f9236c0abf9
Tim Flood <tflood9@gmail.com>
2020-10-15 20:08:47 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO bears
+  (name, age, gender, color, temperament)
+VALUES
+  ("Mr. Chocoloate", 2, "male", "brown", "nice");
+INSERT INTO bears
+  (name, age, gender, color, temperament)
+VALUES
+  ("Rowdy", 2, "male", "brown", "nice");
+INSERT INTO bears
+  (name, age, gender, color, temperament)
+VALUES
+  ("Tabitha", 2, "female", "brown", "nice");
+INSERT INTO bears
+  (name, age, gender, color, temperament)
+VALUES
+  ("Sergeant Brown", 3, "male", "brown", "nice");
+INSERT INTO bears
+  (name, age, gender, color, temperament)
+VALUES
+  ("Melissa", 2, "female", "brown", "nice");
+INSERT INTO bears
+  (name, age, gender, color, temperament)
+VALUES
+  ("Grinch", 3, "male", "brown", "nice");
+INSERT INTO bears
+  (name, age, gender, color, temperament)
+VALUES
+  ("Wendy", 1, "female", "brown", "nice");
+INSERT INTO bears
+  (name, age, gender, color, temperament)
+VALUES
+  (NULL, 4, "male", "brown", "nice");
